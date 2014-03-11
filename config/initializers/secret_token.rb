1cf0b68ec812f0eb811981c1b0ab5c5141361d45
Nick Freeman <nickafreeman@berkeley.edu>
2014-03-10 22:50:35 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OneSquareMeter::Application.config.secret_key_base = 'b33dbb650c8220af3ac8f842a4a41d69841b86b93686488ffadc1e0b5e92de57de14ce9683971a2930d392414e98d5bc5bc30cd4bc9524572b6214a8058e3e6d'
